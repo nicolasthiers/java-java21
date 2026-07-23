024249e930df9f1d6312a88486f7ab750421bc5f
Nicolas Thiers <thiers.nicolas31@gmail.com>
2026-07-23 16:21:26 +0200

    tp03 functions

--- src/test/java/java17/ex06/Function_06_Test.java
+++ src/test/java/java17/ex06/Function_06_Test.java
@@ -17,8 +17,8 @@ public class Function_06_Test {
     // TODO compléter la méthode
     // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
     String formatAge(Supplier<Person> supplier) {
-        // TODO
-        return null;
+
+        return "[age=" + supplier.get().getAge() + "]";
     }
     // end::formatAge[]
 
@@ -26,7 +26,7 @@ public class Function_06_Test {
     @Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+        String result = formatAge(() -> new Person("Jean", "Valjean", 35, "secrat"));
 
         assert result.equals("[age=35]");
     }
