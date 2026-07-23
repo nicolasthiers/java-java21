3696a15b4adb9170e369eaf768e7466bdac1cf32
Nicolas Thiers <thiers.nicolas31@gmail.com>
2026-07-23 11:29:21 +0200

    tp01 j'avais oublié l'exo 2

--- src/test/java/java17/ex02/Method_02_Test.java
+++ src/test/java/java17/ex02/Method_02_Test.java
@@ -14,11 +14,13 @@ public class Method_02_Test {
 
     // tag::IDao[]
     interface IDao {
+
         List<Person> findAll();
 
-        // TODO créer une méthode String format()
-        // TODO la méthode retourne une chaîne de la forme [<nb_personnes> persons]
-        // TODO exemple de résultat : "[14 persons]", "[30 persons]"
+        // Retourne par exemple : "[20 persons]"
+        default String format() {
+            return "[" + findAll().size() + " persons]";
+        }
     }
     // end::IDao[]
 
@@ -32,11 +34,12 @@ public class Method_02_Test {
             return people;
         }
 
-        // TODO redéfinir la méthode String format()
-        // TODO la méthode retourne une chaîne de la forme DaoA[<nb_personnes> persons]
-        // TODO exemple de résultat : "DaoA[14 persons]", "DaoA[30 persons]"
-        // TODO l'implémentation réutilise la méthode format() de l'interface
-
+        // Retourne par exemple : "DaoA[20 persons]"
+        // en réutilisant la méthode default de l'interface
+        @Override
+        public String format() {
+            return getClass().getSimpleName() + IDao.super.format();
+        }
     }
     // end::DaoA[]
 
@@ -45,8 +48,7 @@ public class Method_02_Test {
 
         DaoA daoA = new DaoA();
 
-        // TODO invoquer la méthode format() pour que le test soit passant
-        String result = null;
+        String result = daoA.format();
 
         assert "DaoA[20 persons]".equals(result);
     }
