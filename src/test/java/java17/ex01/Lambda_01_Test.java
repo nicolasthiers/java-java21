bfce57bd3229ea4d7c78e4882d0c554e0b7e7dda
Nicolas Thiers <thiers.nicolas31@gmail.com>
2026-07-23 13:43:30 +0200

    tp02

--- src/test/java/java17/ex01/Lambda_01_Test.java
+++ src/test/java/java17/ex01/Lambda_01_Test.java
@@ -1,5 +1,6 @@
 package java17.ex01;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
 import java17.data.Data;
@@ -39,8 +40,8 @@ public class Lambda_01_Test {
 
         List<Person> personList = Data.buildPersonList(100);
 
-        // TODO result ne doit contenir que des personnes adultes (age >= 18)
-        List<Person> result = filter(personList, null);
+        PersonPredicate pp = p -> p.getAge() >= 18;
+        List<Person> result = filter(personList, pp);
 
         assert result.size() == 83;
 
@@ -56,8 +57,8 @@ public class Lambda_01_Test {
 
         List<Person> personList = Data.buildPersonList(100);
 
-        // TODO result ne doit contenir que des personnes dont le prénom est "first_10"
-        List<Person> result = filter(personList, null);
+        PersonPredicate pp = p -> p.getFirstname().equals("first_10");
+        List<Person> result = filter(personList, pp);
 
         assert result.size() == 1;
         assert result.get(0).getFirstname().equals("first_10");
@@ -73,9 +74,10 @@ public class Lambda_01_Test {
 
         String passwordSha512Hex = "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff";
 
+        PersonPredicate pp = p -> p.getAge() > 49 && DigestUtils.sha512Hex(p.getPassword()).equals(passwordSha512Hex);
         // TODO result ne doit contenir que les personnes dont l'age est > 49 et dont le hash du mot de passe correspond à la valeur de la variable passwordSha512Hex
         // TODO Pour obtenir le hash d'un mot, utiliser la méthode DigestUtils.sha512Hex(mot)
-        List<Person> result = filter(personList, null);
+        List<Person> result = filter(personList, pp);
 
         assert result.size() == 6;
         for (Person person : result) {
