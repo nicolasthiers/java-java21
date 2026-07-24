e330aa324783093b9c4f300a77542b19e8d392bf
Nicolas Thiers <thiers.nicolas31@gmail.com>
2026-07-24 14:05:38 +0200

    tp05 Streams

--- src/test/java/java17/ex02/Stream_02_Test.java
+++ src/test/java/java17/ex02/Stream_02_Test.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertThat;
 
 import java.util.List;
 import java.util.OptionalDouble;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 
@@ -24,7 +26,7 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Trouver la liste des clients associés aux commandes
-		List<Customer> result = null;
+		List<Customer> result = orders.stream().map(o -> o.getCustomer()).toList();
 
 		assertThat(result, hasSize(8));
 	}
@@ -35,7 +37,7 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Compter le nombre de clients associés aux commandes
-		long result = 0;
+		long result = orders.stream().map(o -> o.getCustomer()).count();
 
 		assertThat(result, is(8L));
 	}
@@ -46,7 +48,7 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Trouver la liste des différents clients associés aux commandes (sans doublons)
-		List<Customer> result = null;
+		Set<Customer> result = orders.stream().map(o -> o.getCustomer()).collect(Collectors.toSet());
 
 		assertThat(result, hasSize(2));
 	}
@@ -57,7 +59,7 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Compter le nombre des différents clients associés aux commandes
-		long result = 0L;
+		long result = orders.stream().map(o -> o.getCustomer()).distinct().count();
 
 		assertThat(result, is(2L));
 	}
@@ -69,9 +71,9 @@ public class Stream_02_Test {
 
 		/*
 		 * TODO Calculer le chiffre d'affaires total de la pizzeria (somme des prix des
-		 * commandes)
+		 * TODO commandes)
 		 */
-		double result = 0.0;
+		double result = orders.stream().mapToDouble(order -> order.getPrice()).sum();
 
 		assertThat(result, is(10900.0));
 	}
@@ -84,7 +86,7 @@ public class Stream_02_Test {
 		/*
 		 * TODO Calculer le prix moyen d'une commande
 		 */
-		OptionalDouble result = null;
+		OptionalDouble result = orders.stream().mapToDouble(Order::getPrice).average();
 
 		assertThat(result.isPresent(), is(true));
 		assertThat(result.getAsDouble(), is(1362.5));
